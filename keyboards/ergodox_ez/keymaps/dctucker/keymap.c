63e3deb2ec79925e1cb94921b5a35899aafa0840
Casey Tucker <dctucker@github.com>
2020-03-09 12:12:51 -0700

    LT2FIVE, thumb function keys, caps behavior

--- keyboards/ergodox_ez/keymaps/dctucker/keymap.c
+++ keyboards/ergodox_ez/keymaps/dctucker/keymap.c
@@ -24,23 +24,28 @@ enum custom_keycodes {
 
 #define ZOOMKEY LALT(LCTL(LGUI(KC_Z)))
 #define LT1EQ   LT(1, KC_EQUAL)
+#define LT1KPLS LT(1, KC_KP_PLUS)
 #define LT1S    LT(1, KC_SCOLON)
 #define LT2BS   LT(2, KC_BSLS)
+#define LT2FIVE LT(2, KC_5)
 #define ALTMN   ALGR_T(KC_MINUS)
+#define H12     LALT(LGUI(KC_F12))
+#define H16     LALT(LGUI(KC_F16))
 //#define CTLESC  CTL_T(KC_ESCAPE)
 //RCTL_T(KC_ENTER)
 #define CTLDEL  CTL_T(KC_DELETE)
+#define MOUSE1 KC_MS_BTN1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_ergodox_pretty(
-		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,  LT2BS  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_EQL ,  KC_MINS, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , MONEYQ ,
+		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,  LT2FIVE, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_EQL ,  KC_BSLS, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , MONEYQ ,
 		CTLESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                    KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-		KC_LSFT, KC_Z   , KC_B   , KC_X   , KC_C   , KC_V   , KC_B   ,  KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-		CTLDEL , KC_F1  , LT1EQ  , KC_LALT, KC_LGUI,                                      KC_RGUI, KC_RALT, ALTMN  , KC_LBRC, KC_RBRC,
+		KC_LSFT, KC_Z   , MOUSE1 , KC_X   , KC_C   , KC_V   , KC_B   ,  KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+		CTLDEL , KC_F1  , LT1KPLS, KC_LALT, KC_LGUI,                                      KC_RGUI, KC_RALT, ALTMN  , KC_LBRC, KC_RBRC,
 		                                             KC_HOME, KC_END ,  KC_LEFT, KC_RGHT,
 		                                                      KC_PGUP,  KC_UP  ,
-		                                      KC_SPC , BARS , KC_PGDN,  KC_DOWN, CTLENT , KC_SPC
+		                                    KC_SPC , BARS   , KC_PGDN,  KC_DOWN, CTLENT , KC_SPC
 	),
 	[1] = LAYOUT_ergodox_pretty(
 		_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , _______,  _______, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_DEL ,
@@ -50,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______,                                      KC_P0  , KC_PDOT, KC_PDOT, KC_PENT, _______,
 		                                            RGB_MOD,  _______,  RGB_TOG,  RGB_SLD,
 		                                                      _______,  _______,
-		                                  RGB_VAD,  RGB_VAI,  _______,  _______,  RGB_HUD,  RGB_HUI
+		                                   RGB_VAD, RGB_VAI,  _______,  _______,  RGB_HUD,  RGB_HUI
 	),
 	[2] = LAYOUT_ergodox_pretty(
 		_______, KC_F20 , KC_F21 , _______, _______, _______, _______,  _______, KC_VOLD, KC_VOLU, _______, _______, _______, KC_DEL ,
@@ -63,14 +68,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		                                    _______, _______, _______,  _______, _______, KC_MPLY
 	),
 	[3] = LAYOUT_ergodox_pretty(
-		_______, _______, _______, _______, _______, _______, _______,  KC_PIPE, KC_CIRC, KC_AMPR, KC_PAST, KC_LPRN, KC_RPRN, _______,
+		_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , _______,  KC_PIPE, KC_CIRC, KC_AMPR, KC_PAST, KC_LPRN, KC_RPRN, _______,
 		_______, _______, _______, _______, _______, _______, _______,  KC_PIPE, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_BSLS,
 		_______, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, _______,
 		_______, _______, _______, _______, _______, _______, _______,  _______, BACKN  , KC_MINS, EARROW , ARROW  , COMMENT, _______,
-		_______, _______, _______, _______, _______,                                     _______, _______, _______, KC_LPRN, KC_RPRN,
-		                                             _______, _______,  KC_F14 , KC_F15 ,
-		                                                      _______,  KC_F13 ,
-		                                    _______, _______, _______,  KC_F16 , _______, _______
+		_______, _______, _______, _______, _______,                                      _______, _______, _______, KC_LPRN, KC_RPRN,
+		                                             KC_F10 , KC_F11 ,  KC_F14 , KC_F15 ,
+		                                                      KC_F9  ,  KC_F13 ,
+		                                    H12    , _______, KC_F12 ,  KC_F16 , _______, H16
+	),
+	[4] = LAYOUT_ergodox_pretty(
+		_______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______,
+		                                             _______, _______,  _______, _______,
+		                                                      _______,  _______,
+		                                    H12    , _______, _______,  _______, _______, H16
 	),
 };
 
@@ -94,7 +109,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 bool xtra_sent = true;
 bool ctl_xtra_sent = true;
 uint16_t lctl_timer, rctl_timer;
-bool caps = false;
+//bool caps = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
@@ -120,12 +135,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				unregister_code(KC_LCTL);
 				if( ! ctl_xtra_sent ){
 					if( timer_elapsed(lctl_timer) < 500 ){
+						/*
 						if( caps ){
 							register_code(KC_CAPS);
 							unregister_code(KC_CAPS);
 						} else {
-							send_string("\033");
-						}
+						*/
+						send_string("\033");
+						//}
 					}
 					ctl_xtra_sent = true;
 				}
@@ -137,7 +154,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				ctl_xtra_sent = false;
 				rctl_timer = timer_read();
 				register_code(KC_RCTL);
+				layer_on(4);
 			} else {
+				layer_off(4);
 				unregister_code(KC_RCTL);
 				if( ! ctl_xtra_sent ){
 					if( timer_elapsed(rctl_timer) < 500 ){
@@ -231,10 +250,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void led_set_user(uint8_t usb_led)
 {
 	if( usb_led & (1<<USB_LED_CAPS_LOCK) ){
-		caps = true;
+		//caps = true;
 		ergodox_right_led_3_on();
 	} else {
-		caps = false;
+		//caps = false;
 		ergodox_right_led_3_off();
 	}
 }
